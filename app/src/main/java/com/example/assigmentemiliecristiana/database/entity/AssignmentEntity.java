e23deaa0e1af4a2608695de7176e28de0ac09519
Your Name <limacoelhocristiana@gmial.com>
2022-03-14 11:42:08 +0100

    database Entity

---
+++ app/src/main/java/com/example/assigmentemiliecristiana/database/entity/AssignmentEntity.java
@@ -0,0 +1,118 @@
+package com.example.assigmentemiliecristiana.database.entity;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Entity(tableName = "assignments",
+        foreignKeys =
+        @ForeignKey(
+                entity = StudentEntity.class,
+                parentColumns = "username",
+                childColumns = "owner",
+                onDelete = ForeignKey.CASCADE
+        ),
+        indices = {
+                @Index(
+                        value = {"owner"}
+                )}
+)
+public class AssignmentEntity implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
+    private String name;
+    private String [] type;
+    private String description;
+    private Date date;
+    private String [] status;
+    private String owner;
+
+    public AssignmentEntity() {
+    }
+
+    public AssignmentEntity(String name, String[] type, String description, Date date, String[] status, String owner) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.date = date;
+        this.status = status;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String[] getType() {
+        return type;
+    }
+
+    public void setType(String[] type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String[] getStatus() {
+        return status;
+    }
+
+    public void setStatus(String[] status) {
+        this.status = status;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (!(obj instanceof AssignmentEntity)) return false;
+        AssignmentEntity o = (AssignmentEntity) obj;
+        return o.getId().equals(this.getId());
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ app/src/main/java/com/example/assigmentemiliecristiana/database/entity/StudentEntity.java
@@ -0,0 +1,77 @@
+package com.example.assigmentemiliecristiana.database.entity;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+@Entity(tableName = "students", primaryKeys = {"email"})
+public class StudentEntity {
+
+    @NonNull
+    private String username;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    private String password;
+
+    @Ignore
+    public StudentEntity() {
+    }
+
+    public StudentEntity(@NonNull String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (o == this) return true;
+        if (!(o instanceof StudentEntity)) return false;
+        StudentEntity obj = (StudentEntity) o;
+        return obj.getEmail().equals(this.getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return
+                "username='" + username + '\'' +
+                ", email='" + email + '\'';
+    }
+
+    public int compareTo(@NonNull Object o) {
+        return toString().compareTo(o.toString());
+    }
+
+}
