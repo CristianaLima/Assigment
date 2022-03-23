f833a5aa26af7697104ad9c64e04422913cab665
etdnp <emilierey.teodorodonasciment@students.hevs.ch>
2022-03-23 08:24:54 +0100

    Welcome Page fonctionnelle

--- app/src/main/java/com/example/assigmentemiliecristiana/UI/WelcomePage.java
+++ app/src/main/java/com/example/assigmentemiliecristiana/UI/WelcomePage.java
@@ -1,5 +1,6 @@
 package com.example.assigmentemiliecristiana.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -7,9 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.assigmentemiliecristiana.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WelcomePage extends AppCompatActivity {
 
     ImageView homework_icon;
+    Timer timer;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -17,11 +22,19 @@ public class WelcomePage extends AppCompatActivity {
         setContentView(R.layout.welcome_page);
 
         homework_icon = findViewById(R.id.homework_icon);
-
         homework_icon.setAlpha(0f);
         homework_icon.setTranslationY(50);
-
         homework_icon.animate().alpha(1f).translationYBy(-50).setDuration(1500);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(WelcomePage.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        },3500);
 
     }
 
