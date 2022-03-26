d6d278d0234e4375f82f948dd5b01582f2c97552
etdnp <emilierey.teodorodonasciment@students.hevs.ch>
2022-03-26 19:47:50 +0100

    login page

--- app/src/main/java/com/example/assigmentemiliecristiana/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/assigmentemiliecristiana/adapter/RecyclerAdapter.java
@@ -22,9 +22,9 @@ public class RecyclerAdapter<T> extends RecyclerView.Adapter<RecyclerAdapter.Vie
     private RecyclerViewItemClickListener mListener;
 
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
+/** Provide a reference to the views for each data item
+   * Complex data items may need more than one view per item, and
+    * you provide access to all the views for a data item in a view holder */
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
